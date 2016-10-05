63bb86b82bdd82459bbee3964bfc5be660e0e5fe
FrederikeR <frederike.rueppel@prognostica.de>
2016-10-05 22:24:16 +0200

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -20,18 +20,18 @@ data_subset$date.time <- date.time
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data_subset, {
   # plot1
-  plot(Global_active_power~date.time, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Global_active_power~date.time, type="l", ylab="Global Active Power", xlab="")
   # plot2
-  plot(Voltage~date.time, type="l", ylab="Voltage (volt)", xlab="")
+  plot(Voltage~date.time, type="l", ylab="Voltage", xlab="")
   # plot3
-  plot(Sub_metering_1~date.time, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Sub_metering_1~date.time, type="l", ylab="Energy sub metering", xlab="")
   lines(Sub_metering_2~date.time,col='Red')
   lines(Sub_metering_3~date.time,col='Blue')
   # legend for plot3 
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   # plot4
-  plot(Global_reactive_power~date.time, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
+  plot(Global_reactive_power~date.time, type="l", ylab="Global_reactive_power",xlab="")
 })
 
 ## Saving to file
